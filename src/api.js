dd51d04614431833744a91db236cb67b26a41f5f
Jose <jozylopez27@gmail.com>
2020-01-16 10:59:15 -0600

    added api module

---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+export default {
+    getItems
+};
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[joselopez]';
+
+function getItems() {
+    return Promise.resolve('A successful response!');
+}
